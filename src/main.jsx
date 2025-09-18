66c068c04118925f681ee1f12d79d90a0cbf58d3
Wicayonima-Reborn <alhamdulillahsehatwkwk@gmail.com>
2025-09-18 11:17:49 +0700

    Switch to footer to Footer

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import Navbar from './components/Navbar.jsx'
 import 'remixicon/fonts/remixicon.css'
-import Footer from './components/footer.jsx'
+import Footer from './components/Footer.jsx'
 import Preloader from './components/Preloader.jsx'
 import 'animate.css';
 import AOS from 'aos';
